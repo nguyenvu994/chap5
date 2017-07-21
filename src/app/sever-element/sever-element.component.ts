32ca9143ffaecbd102a0ed571aba95ead3c30314
nguyen vu <nguyen vu>
2017-07-20 20:15:17 -0700

    leaving

--- src/app/sever-element/sever-element.component.ts
+++ src/app/sever-element/sever-element.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./sever-element.component.css']
 })
 export class SeverElementComponent implements OnInit {
-  @Input () element: {type: string, name: string, content: string};
+  @Input ('srvElement') element: {type: string, name: string, content: string};
   constructor() { }
 
   ngOnInit() {
